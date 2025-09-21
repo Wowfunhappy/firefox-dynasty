30c6af0d182a30a723bd63eb5e80a561b9648aca
broly <broly@mac.com>
2025-09-21 13:56:48 -0600

    restore (new) mach ipc backend after finding issue (rleez nutz)
    looks like mr @blueboxd in his blue window's changes stick now.
    
    the difference seemed to be that we don't need to drain the messages for older macs in
    PRocessEvents when receiving an EVFILT_MACHPORT event like chromium does.
    
    it seems that this draining was the issue, as the removal of the mach_msg_receive(&wakeup_buffer_header)
    bits restores functionality the way we would expect.
    
    may revert if this is wrong.

--- ipc/chromium/src/base/message_pump_kqueue.cc
+++ ipc/chromium/src/base/message_pump_kqueue.cc
@@ -21,27 +21,15 @@ namespace base {
 
 namespace {
 
-// Prior to macOS 10.12, a kqueue could not watch individual Mach ports, only
-// port sets. MessagePumpKqueue will directly use Mach ports in the kqueue if
-// it is possible.
-bool KqueueNeedsPortSet() {
-#ifdef XP_DARWIN 
-  static const bool kqueue_needs_port_set = !nsCocoaFeatures::OnSierraOrLater();
-  return kqueue_needs_port_set;
-#else
-  return false;
-#endif
-}
-
 // Prior to macOS 10.14, kqueue timers may spuriously wake up, because earlier
 // wake ups race with timer resets in the kernel. As of macOS 10.14, updating a
 // timer from the thread that reads the kqueue does not cause spurious wakeups.
 // Note that updating a kqueue timer from one thread while another thread is
 // waiting in a kevent64 invocation is still (inherently) racy.
 bool KqueueTimersSpuriouslyWakeUp() {
-#ifdef XP_DARWIN 
-  static const bool kqueue_timers_spuriously_wakeup = !nsCocoaFeatures::OnMojaveOrLater();
-  return kqueue_timers_spuriously_wakeup;
+#ifdef XP_DARWIN
+  static const bool kqueue_needs_port_set = !nsCocoaFeatures::OnSierraOrLater();
+  return kqueue_needs_port_set;
 #else
   // This still happens on iOS15.
   return true;
@@ -141,12 +129,12 @@ MessagePumpKqueue::MessagePumpKqueue() : kqueue_(kqueue()) {
   // Specify the wakeup port event to directly receive the Mach message as part
   // of the kevent64() syscall.
   kevent64_s event{};
-  if (KqueueNeedsPortSet()) {
-    mach_port_t compat_port;
+  if (KqueueTimersSpuriouslyWakeUp()) {
+    mach_port_t port_set;
     kr = mach_port_allocate(mach_task_self(), MACH_PORT_RIGHT_PORT_SET,
-                            &compat_port);
+                            &port_set);
     CHECK(kr == KERN_SUCCESS) << "mach_port_allocate PORT_SET";
-    port_set_.reset(compat_port);
+    port_set_.reset(port_set);
     kr = mach_port_insert_member(mach_task_self(), wakeup_.get(),
                                  port_set_.get());
     CHECK(kr == KERN_SUCCESS) << "mach_port_insert_member";
@@ -242,7 +230,7 @@ bool MessagePumpKqueue::WatchMachReceivePort(
     return false;
   }
 
-  if (KqueueNeedsPortSet()) {
+  if (KqueueTimersSpuriouslyWakeUp()) {
     kern_return_t kr =
         mach_port_insert_member(mach_task_self(), port, port_set_.get());
     if (kr != KERN_SUCCESS) {
@@ -322,7 +310,7 @@ bool MessagePumpKqueue::StopWatchingMachPort(
   mach_port_t port = controller->port();
   controller->Reset();
   port_controllers_.Remove(port);
-  if (KqueueNeedsPortSet()) {
+  if (KqueueTimersSpuriouslyWakeUp()) {
     kern_return_t kr =
         mach_port_extract_member(mach_task_self(), port, port_set_.get());
     if (kr != KERN_SUCCESS) {
@@ -436,21 +424,10 @@ bool MessagePumpKqueue::ProcessEvents(Delegate* delegate, int count) {
         }
       }
     } else if (event->filter == EVFILT_MACHPORT) {
-      mach_port_t port = KqueueNeedsPortSet() ? event->data : event->ident;
-
+      mach_port_t port = KqueueTimersSpuriouslyWakeUp() ? static_cast<mach_port_t>(event->data) : static_cast<mach_port_t>(event->ident);
       if (port == wakeup_.get()) {
         // The wakeup event has been received, do not treat this as "doing
         // work", this just wakes up the pump.
-        if (KqueueNeedsPortSet()) {
-          // When using the kqueue directly, the message can be received
-          // straight into a buffer that was created when adding the event.
-          // But when using a port set, the message must be drained manually.
-          wakeup_buffer_.header.msgh_local_port = port;
-          wakeup_buffer_.header.msgh_size = sizeof(wakeup_buffer_);
-          kern_return_t kr = mach_msg_receive(&wakeup_buffer_.header);
-          DLOG_IF(ERROR, kr != KERN_SUCCESS)
-              << "mach_msg_receive wakeup" << mach_error_string(kr);
-        }
         continue;
       }
 
--- ipc/glue/GeckoChildProcessHost.cpp
+++ ipc/glue/GeckoChildProcessHost.cpp
@@ -1894,8 +1894,6 @@ RefPtr<ProcessLaunchPromise> BaseProcessLauncher::Launch(
     if (auto* handle = std::get_if<UniqueFileHandle>(&clientHandle)) {
       geckoargs::sIPCHandle.Put(std::move(*handle), mChildArgs);
     }
-// it ain't workin sorry nika
-/**
 #ifdef XP_DARWIN
     // NOTE: We don't support passing UniqueMachReceiveRight instances on the
     // command line, so clientHandle must be a send right.
@@ -1903,7 +1901,6 @@ RefPtr<ProcessLaunchPromise> BaseProcessLauncher::Launch(
       geckoargs::sIPCPort.Put(std::move(*port), mChildArgs);
     }
 #endif
-*/
     else {
       MOZ_ASSERT_UNREACHABLE();
       return ProcessLaunchPromise::CreateAndReject(LaunchError("BadPipeType"),
